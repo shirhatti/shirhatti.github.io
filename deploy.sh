fb85c964db8b84cfc501552a5873206da4dd9733
Sourabh Shirhatti <shirhatti@outlook.com>
2016-04-18 02:43:03 -0700

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
-echo "Deploy not implemented"
+  cd public
+  FULL_REPO="https://$GH_TOKEN@github.com/shirhatti/shirhatti.github.io.git"
+  git init
+  git config user.name "Travis-CI"
+  git config user.email "sourabh@shirhatti.com"
+  git add -A
+  git commit -m "Deploy to GitHub Pages"
+  git push --force --quiet $FULL_REPO master
