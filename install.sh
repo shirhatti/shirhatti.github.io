99899847f7ce47681429049ce758f4e5ab3aa78f
Sourabh Shirhatti <shirhatti@outlook.com>
2016-04-18 02:17:25 -0700

    Add travis

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Install hugo
+curl -sSL https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz -O hugo.tar.gz
+tar -zxvf hugo.tar.gz
+mv hugo_0.15_linux_amd64/hugo_0.15_linux_amd64 ./hugo
+
+
+
