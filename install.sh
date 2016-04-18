963b84659fe288c6eb7fae79b74389b519b5fd8f
Sourabh Shirhatti <shirhatti@outlook.com>
2016-04-18 02:19:51 -0700

    Typo in curl

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Install hugo
-curl -sSL https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz -O hugo.tar.gz
+curl -sSL https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz -o hugo.tar.gz
 tar -zxvf hugo.tar.gz
 mv hugo_0.15_linux_amd64/hugo_0.15_linux_amd64 ./hugo
 
