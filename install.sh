33559b94b251ba8940a1a06f1b57411b250772a4
Sourabh Shirhatti <shirhatti@outlook.com>
2016-04-18 02:39:38 -0700

    Make install portable

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
-# Install hugo
-curl -sSL https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz -o hugo.tar.gz
-tar -zxf hugo.tar.gz
-mv hugo_0.15_linux_amd64/hugo_0.15_linux_amd64 ./hugo
+#!/bin/sh
+
+if test `uname` = Darwin; then
+    curl -sSL https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_darwin_amd64.zip -o hugo.tar.gz
+    tar -zxf hugo.tar.gz
+    mv hugo_0.15_darwin_amd64/hugo_0.15_darwin_amd64 ./hugo
+elif test `uname` = Linux; then
+    curl -sSL https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz -o hugo.tar.gz
+    tar -zxf hugo.tar.gz
+    mv hugo_0.15_linux_amd64/hugo_0.15_linux_amd64 ./hugo
+fi
 
 
 
