0bf1fdd05fb9210dd71cdb1954525ef386378509
Sourabh Shirhatti <shirhatti@outlook.com>
2016-04-18 02:26:58 -0700

    Remove verbose flag for tar

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Install hugo
 curl -sSL https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz -o hugo.tar.gz
-tar -zxvf hugo.tar.gz
+tar -zxf hugo.tar.gz
 mv hugo_0.15_linux_amd64/hugo_0.15_linux_amd64 ./hugo
 
 
