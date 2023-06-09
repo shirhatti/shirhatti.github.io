078bb7ae470d61093ab7f268483c02e0a3027e8b
Sourabh Shirhatti <shirhatti@outlook.com>
2023-06-09 05:31:26 +0000

    Update to GH workflow

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-  cd public
-  FULL_REPO="https://$GH_TOKEN@github.com/shirhatti/shirhatti.github.io.git"
-  git init
-  git config user.name "Travis-CI"
-  git config user.email "sourabh@shirhatti.com"
-  git add -A
-  git commit -m "Deploy to GitHub Pages"
-  git push --force --quiet $FULL_REPO master
